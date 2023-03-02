3e3b8b81273a0a8f715e0f0cf930c92bab2b7995
Marcos Henrique <marcossistemas.ac@gmail.com>
2023-03-02 00:07:56 -0300

    JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,10 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'Marcos'
+  const newname = name.toUpperCase()
+
+  function sum(a, b){
+    return a + b 
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-    <h1>Olá React!</h1>
-    <p>Meu Primeiro App</p>
+      <h1>Alterando o JSX</h1>
+      <p>Ola, {newname}</p>
+      <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
   );
 }
